38c036e0a7af37385d2057a1eb85760b8b998aeb
Zhuye <z.che@outlook.com>
2022-02-15 08:16:36 -0600

    Added validation in University model

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,2 +1,5 @@
 class University < ApplicationRecord
+  validates :name, :city, :state, :zip, :program, presence: true
+  validates :name, uniqueness: true
+  validates :rank, numericality: { only_integer: true }
 end
