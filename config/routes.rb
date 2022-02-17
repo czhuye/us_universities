d13e86263922961c72a4fdd9388495800e7e6f99
Zhuye <z.che@outlook.com>
2022-02-17 03:25:22 -0600

    modified _university view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'states/index'
-  get 'states/show'
-  get "home/index"
+  # get "states/index"
+  # get "states/show"
+  # get "home/index"
   resources :universities
+  resources :states, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
